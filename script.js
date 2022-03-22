d58b53ec07c4dd425d8068e1d58322dc8bf851f2
lingeshwaran <94908980+Lingeshwaranlee@users.noreply.github.com>
2022-03-22 19:49:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+setTimeout(() => {
+    document.getElementById('display').innerText = "10";
+
+    setTimeout(() => {
+        document.getElementById('display').innerText = "9";
+
+        setTimeout(() => {
+            document.getElementById('display').innerText = "8";
+
+            setTimeout(() => {
+                document.getElementById('display').innerText = "7";
+
+                setTimeout(() => {
+                    document.getElementById('display').innerText = "6";
+
+                    setTimeout(() => {
+                        document.getElementById('display').innerText = "5";
+
+                        setTimeout(() => {
+                            document.getElementById('display').innerText = "4";
+
+                            setTimeout(() => {
+                                document.getElementById('display').innerText = "3";
+
+                                setTimeout(() => {
+                                    document.getElementById('display').innerText = "2";
+
+                                    setTimeout(() => {
+                                        document.getElementById('display').innerText = "1";
+
+                                        setTimeout(() => {
+                                            document.getElementById('display').innerHTML = `<div class="text-center">
+                                                                                           <img src="https://thumbs.dreamstime.com/b/happy-birthday-cupcake-celebration-message-160558421.jpg" class="img rounded img-thumbnail" alt="img">
+                                                                                               <h3 class="display"> Happy birthday Day...</h3> 
+                                                                                                            </div> `;
+                                        }, 1000)
+                                    }, 1000)
+                                }, 1000)
+                            }, 1000)
+                        }, 1000)
+                    }, 1000)
+                }, 1000)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
